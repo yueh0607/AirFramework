e35097c08eb7472bb18efcb737ca5ed7350eafe1
yueh0607 <imyueh@foxmail.com>
2023-01-22 14:21:12 +0800

    重MessageDispatcher，更名为Event相关内容

---
+++ Framework/Core/Message/interface/IEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirFramework.Assets.Framework.Core.Message
+{
+    public interface IHandler
+    {
+
+    }
+    public interface IEventHandler:IHandler
+    {
+        void HandleEvent(IEvent globalEvent);
+    }
+    public interface IEventHandler<T>:IHandler
+    {
+        T HandleEvent(IEvent globalEvent);
+    }
+    
+}
