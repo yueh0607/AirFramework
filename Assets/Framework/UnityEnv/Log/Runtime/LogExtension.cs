af3557748e0c520c10583a3b8b93f18623644605
yueh0607 <imyueh@foxmail.com>
2023-03-10 15:30:16 +0800

    计划移除Log模块

--- Assets/Framework/UnityEnv/Log/Runtime/LogExtension.cs
+++ Assets/Framework/UnityEnv/Log/Runtime/LogExtension.cs
@@ -26,31 +26,43 @@ namespace AirFramework
 
         All
     }
+
+
+
     public static partial class MessageExtensions
     {
+        /// <summary>
+        /// 输出普通日志
+        /// </summary>
+        /// <param name="any"></param>
         public static void L(this object any)
         {
             Debug.Log(any);
         }
+        /// <summary>
+        /// 输出警告日志
+        /// </summary>
+        /// <param name="any"></param>
         public static void W(this object any)
         {
             Debug.LogWarning(any);
         }
+        /// <summary>
+        /// 输出错误日志
+        /// </summary>
+        /// <param name="any"></param>
         public static void E(this object any)
         {
             Debug.LogError(any);
         }
-
+        /// <summary>
+        /// 抛出异常到控制台
+        /// </summary>
+        /// <param name="ex"></param>
         public static void Throw(this Exception ex)
         {
             Debug.LogException(ex);
         }
 
-
-        public static T Throw<T>(this Exception ex)
-        {
-            Throw(ex);
-            return default;
-        }
     }
 }
