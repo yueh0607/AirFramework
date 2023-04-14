e673187c96902b21b374f34402ddd112224a6eb1
yueh0607 <imyueh@foxmail.com>
2023-04-14 20:53:05 +0800

    Code 清理

--- Assets/Framework/Runtime/UnityEnv/Config/Singleton/ScriptableObjectSingleton.cs
+++ Assets/Framework/Runtime/UnityEnv/Config/Singleton/ScriptableObjectSingleton.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
@@ -8,22 +6,7 @@ using UnityEngine;
 namespace AirFramework
 {
 
-    [AttributeUsage(AttributeTargets.Class)]
-    public class FilePathAttribute : Attribute
-    {
-        internal string FilePath;
 
-        /// <summary>
-        /// 单例存放路径
-        /// </summary>
-        /// <param name="path">相对 Project 路径</param>
-        public FilePathAttribute(string path)
-        {
-            if (string.IsNullOrEmpty(path)) throw new ArgumentException("Invalid relative path (it is empty)");
-            if (path[0] == '/') path = path[1..];
-            FilePath = path;
-        }
-    }
     public class ScriptableSingleton<T> : ScriptableObject where T : ScriptableObject
     {
 
