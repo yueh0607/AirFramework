e3fd128383dddfb346a10e9eacaaefcdfcc09353
yueh0607 <imyueh@foxmail.com>
2023-04-20 14:04:50 +0800

    抽离BindableBase

--- Assets/Framework/Runtime/Core/Modules/Tools/BindableProperty/BindablePort.cs
+++ Assets/Framework/Runtime/Core/Modules/Tools/BindableProperty/BindablePort.cs
@@ -6,13 +6,11 @@ using System.Threading.Tasks;
 
 namespace AirFramework
 {
-    public class BindablePort<T>:IValueChanged<T>
+    public class BindablePort<T>:BindableBase<T>
     {
-        /// <summary>
-        /// 属性值：当属性值发生变化时，通知属性变更
-        /// </summary>
 
-        public T Value
+        public override event PropertyChangedEvent<T> OnValueChanged;
+        public override T Value
         {
             get
             {
@@ -30,61 +28,6 @@ namespace AirFramework
                 }
             }
         }
-
-        /// <summary>
-        /// 事件列表：属性值变更时触发事件
-        /// </summary>
-        public event PropertyChangedEvent<T> OnValueChanged;
-
-        /// <summary>
-        /// 绑定两个属性
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        public static void Bind(IValueChanged<T> a, IValueChanged<T> b)
-        {
-            //这里的实现依赖于相同值不会调用ValueChanged事件，所以才能避免循环调用
-            a.OnValueChanged += (oldV, newV) =>
-            {
-                b.Value = newV;
-            };
-            b.OnValueChanged += (oldV, newV) =>
-            {
-                a.Value = newV;
-            };
-        }
-        /// <summary>
-        /// 绑定到属性
-        /// </summary>
-        /// <param name="target"></param>
-        public void BindTo(IValueChanged<T> target)
-        {
-            target.OnValueChanged += (oldV, newV) =>
-            {
-                this.Value = newV;
-            };
-        }
-        /// <summary>
-        /// 从属性绑定，将当前属性绑定到
-        /// </summary>
-        /// <param name="target"></param>
-        public void BindFrom(IValueChanged<T> target)
-        {
-            this.OnValueChanged += (oldV, newV) =>
-            {
-                target.Value = newV;
-            };
-        }
-        /// <summary>
-        /// 绑定当前与某个属性
-        /// </summary>
-        /// <param name="target"></param>
-        public void Bind(IValueChanged<T> target)
-        {
-            Bind(this, target);
-        }
-
-    
 
         public BindablePort(Action<T> setter, Func<T> getter)
         {
--- Assets/Framework/Runtime/Core/Modules/Tools/BindableProperty/BindableProperty.cs
+++ Assets/Framework/Runtime/Core/Modules/Tools/BindableProperty/BindableProperty.cs
@@ -15,7 +15,7 @@ namespace AirFramework
     /// 可绑定监听事件的属性
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public sealed class BindableProperty<T> : PoolableObject<BindableProperty<T>>,IValueChanged<T>
+    public sealed class BindableProperty<T> :BindableBase<T>,IPoolable
     {
         private T value;
 
@@ -23,7 +23,7 @@ namespace AirFramework
         /// 属性值：当属性值发生变化时，通知属性变更
         /// </summary>
 
-        public T Value
+        public override T Value
         {
             get => value;
             set
@@ -40,55 +40,8 @@ namespace AirFramework
         /// <summary>
         /// 事件列表：属性值变更时触发事件
         /// </summary>
-        public event PropertyChangedEvent<T> OnValueChanged ;
+        public override event PropertyChangedEvent<T> OnValueChanged ;
 
-        /// <summary>
-        /// 绑定两个属性
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        public static void Bind(IValueChanged<T> a,IValueChanged<T> b)
-        {
-          //这里的实现依赖于相同值不会调用ValueChanged事件，所以才能避免循环调用
-            a.OnValueChanged += (oldV, newV) =>
-            {
-                b.Value = newV;
-            };
-            b.OnValueChanged += (oldV, newV) =>
-            {
-                a.Value = newV;
-            };
-        }
-        /// <summary>
-        /// 绑定到属性
-        /// </summary>
-        /// <param name="target"></param>
-        public void BindTo(IValueChanged<T> target)
-        {
-            target.OnValueChanged += (oldV, newV) =>
-            {
-                this.Value = newV;
-            };
-        }
-        /// <summary>
-        /// 从属性绑定，将当前属性绑定到
-        /// </summary>
-        /// <param name="target"></param>
-        public void BindFrom(IValueChanged<T> target)
-        {
-            this.OnValueChanged += (oldV, newV) =>
-            {
-                target.Value = newV;
-            };
-        }
-        /// <summary>
-        /// 绑定当前与某个属性
-        /// </summary>
-        /// <param name="target"></param>
-        public void Bind(IValueChanged<T> target)
-        {
-            Bind(this, target);
-        }
 
         /// <summary>
         /// 初始化：使用初始值初始化可绑定属性,触发实际
@@ -108,47 +61,18 @@ namespace AirFramework
             this.value = default;
         }
 
-        public override void OnAllocate()
+        public void OnAllocate()
         {
             value = default;
         }
 
-        public override void OnRecycle()
+        public void OnRecycle()
         {
             OnValueChanged = null;
             value = default;
         }
 
-
-        private Func<T> getter = null;
-        private Action<T> setter = null;
-        /// <summary>
-        /// 获取Value的Getter
-        /// </summary>
-        public Func<T> Getter
-        {
-            get
-            {
-                if (getter == null)
-                {
-                    getter = () => Value;
-                }
-                return getter;
-            }
-        }
-        /// <summary>
-        /// 获取Value的Setter
-        /// </summary>
-        public Action<T> Setter
-        {
-            get
-            {
-                if (setter == null)
-                {
-                    setter = (value) => { Value = value; };
-                }
-                return setter;
-            }
-        }
+        public IObjectPool ThisPool { get ; set ; }
+        public bool IsRecycled { get ; set; }
     }
 }
