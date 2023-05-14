d63784fc77d5a4840ba4990f96f9952f55ba5102
yueh0607 <imyueh@foxmail.com>
2023-05-14 20:06:39 +0800

    PurePool注释

---
+++ Assets/Framework/Runtime/UnityEnv/Basement/UnitGameObject/PoolableGameObject.cs
@@ -0,0 +1,21 @@
+﻿/***********************************************************************************
+ * Author      : yueh0607
+ * Version     : 2021.3.22f1c1
+ * Date        : 2023/5/14 20:02:55
+ * Description : Describe the function here.
+************************************************************************************/
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AirFramework;
+
+namespace MyNamespace
+{
+    public class PoolableGameObject
+    {
+        
+    }
+}
+
