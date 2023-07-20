bdaae68af66c7a7ddd536516b2178b504fcea88c
yueh0607 <imyueh@foxmail.com>
2023-07-20 09:49:36 +0800

    添加注释，移除stringEvent支持

--- Assets/Framework/Runtime/Core/Message/Extensions/SyncPublisher/Extensions_TryCall.cs
+++ Assets/Framework/Runtime/Core/Message/Extensions/SyncPublisher/Extensions_TryCall.cs
@@ -4,7 +4,13 @@ namespace AirFramework
 {
     public static partial class MessageExtensions
     {
-
+        /// <summary>
+        /// 对指定对象进行TryCall,返回最后一个结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCall<T1>(this IOperatorOut<ICallEvent<T1>> container, out T1 result)
         {
             result= default;
@@ -18,7 +24,13 @@ namespace AirFramework
             events.Reset();
             return true;
         }
-
+        /// <summary>
+        /// 对指定对象进行TryCall,返回最后一个结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCall<T1, T2>(this IOperatorOut<ICallEvent<T1, T2>> container, T1 arg1, out T2 result)
         {
             result = default;
@@ -32,6 +44,13 @@ namespace AirFramework
             events.Reset();
             return true;
         }
+        /// <summary>
+        /// 对指定对象进行TryCall,返回最后一个结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCall<T1, T2, T3>(this IOperatorOut<ICallEvent<T1, T2, T3>> container, T1 arg1, T2 arg2, out T3 result)
         {
             result = default;
@@ -45,6 +64,13 @@ namespace AirFramework
             events.Reset();
             return true;
         }
+        /// <summary>
+        /// 对指定对象进行TryCall,返回最后一个结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCall<T1, T2, T3, T4>(this IOperatorOut<ICallEvent<T1, T2, T3, T4>> container, T1 arg1, T2 arg2, T3 arg3, out T4 result)
         {
             result = default;
@@ -59,6 +85,13 @@ namespace AirFramework
             return true;
         }
 
+        /// <summary>
+        /// 对指定对象进行TryCall,返回最后一个结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCall<T1, T2, T3, T4, T5>(this IOperatorOut<ICallEvent<T1, T2, T3, T4, T5>> container, T1 arg1, T2 arg2, T3 arg3, T4 arg4, out T5 result)
         {
             result = default;
@@ -72,7 +105,13 @@ namespace AirFramework
             events.Reset();
             return true;
         }
-
+        /// <summary>
+        /// 对指定对象进行TryCall,返回最后一个结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCall<T1, T2, T3, T4, T5, T6>(this IOperatorOut<ICallEvent<T1, T2, T3, T4, T5, T6>> container, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, out T6 result)
         {
             result = default;
--- Assets/Framework/Runtime/Core/Message/Extensions/SyncPublisher/Extensions_TryCallAll.cs
+++ Assets/Framework/Runtime/Core/Message/Extensions/SyncPublisher/Extensions_TryCallAll.cs
@@ -4,14 +4,20 @@ namespace AirFramework
 {
     public static partial class MessageExtensions
     {
-
+        /// <summary>
+        /// 对指定对象进行TryCall,返回全部结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCallAll<T1>(this IOperatorOut<ICallEvent<T1>> container, out UnitList<T1> result)
         {
             UnitList<T1> _result = Framework.Pool.Allocate<UnitList<T1>>();
             result = _result;
-            if (container is null)return false;
+            if (container is null) return false;
             var events = ((MessageOperatorBox<IMessage>)container)?.Value;
-            if (events == null || events.Count == 0)return false;
+            if (events == null || events.Count == 0) return false;
             while (events.GetNext(out var dele))
             {
                 _result.Value.Add((dele as Func<T1>).Invoke());
@@ -19,21 +25,34 @@ namespace AirFramework
             events.Reset();
             return true;
         }
-
+        /// <summary>
+        /// 对指定对象进行TryCall,返回全部结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCallAll<T1, T2>(this IOperatorOut<ICallEvent<T1, T2>> container, T1 arg1, out UnitList<T2> result)
         {
             UnitList<T2> _result = Framework.Pool.Allocate<UnitList<T2>>();
             result = _result;
-            if (container is null)return false;
+            if (container is null) return false;
             var events = ((MessageOperatorBox<IMessage>)container)?.Value;
             if (events == null || events.Count == 0) return false;
             while (events.GetNext(out var dele))
             {
-                _result.Value.Add((dele as Func<T1,T2>).Invoke(arg1));
+                _result.Value.Add((dele as Func<T1, T2>).Invoke(arg1));
             }
             events.Reset();
             return true;
         }
+        /// <summary>
+        /// 对指定对象进行TryCall,返回全部结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCallAll<T1, T2, T3>(this IOperatorOut<ICallEvent<T1, T2, T3>> container, T1 arg1, T2 arg2, out UnitList<T3> result)
         {
             UnitList<T3> _result = Framework.Pool.Allocate<UnitList<T3>>();
@@ -48,7 +67,13 @@ namespace AirFramework
             events.Reset();
             return true;
         }
-
+        /// <summary>
+        /// 对指定对象进行TryCall,返回全部结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCallAll<T1, T2, T3, T4>(this IOperatorOut<ICallEvent<T1, T2, T3, T4>> container, T1 arg1, T2 arg2, T3 arg3, out UnitList<T4> result)
         {
             UnitList<T4> _result = Framework.Pool.Allocate<UnitList<T4>>();
@@ -63,7 +88,13 @@ namespace AirFramework
             events.Reset();
             return true;
         }
-
+        /// <summary>
+        /// 对指定对象进行TryCall,返回全部结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCallAll<T1, T2, T3, T4, T5>(this IOperatorOut<ICallEvent<T1, T2, T3, T4, T5>> container, T1 arg1, T2 arg2, T3 arg3, T4 arg4, out UnitList<T5> result)
         {
             UnitList<T5> _result = Framework.Pool.Allocate<UnitList<T5>>();
@@ -78,7 +109,13 @@ namespace AirFramework
             events.Reset();
             return true;
         }
-
+        /// <summary>
+        /// 对指定对象进行TryCall,返回全部结果
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        /// <exception cref="CallNullEventException"></exception>
         public static bool TryCallAll<T1, T2, T3, T4, T5, T6>(this IOperatorOut<ICallEvent<T1, T2, T3, T4, T5, T6>> container, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, out UnitList<T6> result)
         {
             UnitList<T6> _result = Framework.Pool.Allocate<UnitList<T6>>();
