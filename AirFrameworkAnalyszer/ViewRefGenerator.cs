13d8b4d4540c4067d2d3c5329724c7eab2a508df
yueh0607 <imyueh@foxmail.com>
2023-08-02 17:28:15 +0800

    no message

--- AirFrameworkAnalyszer/ViewRefGenerator.cs
+++ AirFrameworkAnalyszer/ViewRefGenerator.cs
@@ -1,62 +1,111 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Diagnostics;
-using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
 
-namespace AirFrameworkAnalyszer
+namespace CodeAnalyzer
 {
-    [Generator(LanguageNames.CSharp)]
-    public class MessageAnalyser : ISourceGenerator
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MyAnalyzer : DiagnosticAnalyzer
     {
-        void ISourceGenerator.Execute(GeneratorExecutionContext context)
+        private static DiagnosticDescriptor Rule1 = new DiagnosticDescriptor(
+            "RID001", "InterfaceName", "The generic parameter type of LifeCycleHandler should be related to interface name", "Naming", DiagnosticSeverity.Warning, true);
+
+        private static DiagnosticDescriptor Rule2 = new DiagnosticDescriptor(
+            "RID002", "MethodName", "The method name should be related to interface name", "Naming", DiagnosticSeverity.Warning, true);
+
+        private static DiagnosticDescriptor Rule3 = new DiagnosticDescriptor(
+            "RID003", "GenericType", "The generic type parameter should be consistent with interface", "Generic", DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule1, Rule2, Rule3);
+
+        public override void Initialize(AnalysisContext context)
         {
-            // 获取当前项目中所有的语法树
-            var syntaxTrees = context.Compilation.SyntaxTrees;
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeClass, SyntaxKind.ClassDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeInterface, SyntaxKind.InterfaceDeclaration);
+        }
 
-            // 查找满足条件的partial类并为其添加HelloWorld方法
-            foreach (var syntaxTree in syntaxTrees)
+        private void AnalyzeClass(SyntaxNodeAnalysisContext context)
+        {
+            var classDeclaration = (ClassDeclarationSyntax)context.Node;
+
+            // Check Rule 1
+            if (IsViolateRule1(classDeclaration))
             {
-                // 获取语法树的根节点
-                var root = syntaxTree.GetRoot();
+                Diagnostic diagnostic = Diagnostic.Create(Rule1, classDeclaration.Identifier.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
 
-                // 遍历根节点找到类声明
-                var classDeclarations = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
-
-                foreach (var classDeclaration in classDeclarations)
-                {
-                    // 获取类的符号信息
-                    var semanticModel = context.Compilation.GetSemanticModel(syntaxTree);
-                    var classSymbol = semanticModel.GetDeclaredSymbol(classDeclaration);
-
-                    if (classSymbol != null &&
-                        classSymbol.Interfaces.Any(i => i.Name == "IView") &&
-                        classDeclaration.Identifier.ValueText.EndsWith("View"))
-                    {
-                        // 创建HelloWorld方法
-                        var helloWorldMethod = SyntaxFactory.MethodDeclaration(
-                            SyntaxFactory.ParseTypeName("void"), "HelloWorld")
-                            .WithModifiers(SyntaxFactory.TokenList(SyntaxFactory.Token(SyntaxKind.PublicKeyword), SyntaxFactory.Token(SyntaxKind.StaticKeyword)))
-                            .WithBody(SyntaxFactory.Block(SyntaxFactory.ParseStatement("Console.WriteLine(\"Hello, World!\");")));
-
-                        // 将HelloWorld方法添加到类中
-                        var newClassDeclaration = classDeclaration.AddMembers(helloWorldMethod);
-
-                        // 替换语法树中的类声明
-                        root = root.ReplaceNode(classDeclaration, newClassDeclaration);
-                    }
-                }
-
-                // 将修改后的语法树添加到生成器上下文
-                context.AddSource("modifiedSource.cs", root.SyntaxTree.GetText().ToString());
+            // Check Rule 3
+            if (IsViolateRule3(classDeclaration))
+            {
+                Diagnostic diagnostic = Diagnostic.Create(Rule3, classDeclaration.Identifier.GetLocation());
+                context.ReportDiagnostic(diagnostic);
             }
         }
 
-        void ISourceGenerator.Initialize(GeneratorInitializationContext context)
+        private void AnalyzeInterface(SyntaxNodeAnalysisContext context)
         {
-        
+            var interfaceDeclaration = (InterfaceDeclarationSyntax)context.Node;
+
+            // Check Rule 2
+            foreach (var member in interfaceDeclaration.Members)
+            {
+                if (member is MethodDeclarationSyntax method)
+                {
+                    if (IsViolateRule2(interfaceDeclaration.Identifier.Text, method.Identifier.Text))
+                    {
+                        Diagnostic diagnostic = Diagnostic.Create(Rule2, method.Identifier.GetLocation());
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
+            }
+        }
+
+        private bool IsViolateRule1(ClassDeclarationSyntax classDeclaration)
+        {
+            // 获取基类的符号信息
+            var semanticModel = context.Compilation.GetSemanticModel(classDeclaration.SyntaxTree);
+            var baseType = classDeclaration.BaseList?.Types.FirstOrDefault()?.Type;
+            var interfaceType = semanticModel.GetTypeInfo(baseType).Type as INamedTypeSymbol;
+
+            // 判断基类的泛型参数与接口名是否无关
+            if (interfaceType != null && interfaceType.TypeArguments.Length > 0)
+            {
+                var interfaceName = interfaceType.TypeArguments[0].Name;
+                var genericArgName = classDeclaration.TypeParameterList.Parameters.FirstOrDefault()?.Identifier.Text;
+                return !interfaceName.StartsWith(genericArgName);
+            }
+
+            return false;
+        }
+
+        private bool IsViolateRule2(string interfaceName, string methodName)
+        {
+            // 判断方法名与接口名是否无关
+            return !methodName.StartsWith(interfaceName);
+        }
+
+        private bool IsViolateRule3(ClassDeclarationSyntax classDeclaration)
+        {
+            // 获取基类的符号信息
+            var semanticModel = context.Compilation.GetSemanticModel(classDeclaration.SyntaxTree);
+            var baseType = classDeclaration.BaseList?.Types.FirstOrDefault()?.Type;
+            var interfaceType = semanticModel.GetTypeInfo(baseType).Type as INamedTypeSymbol;
+
+            // 判断泛型参数是否与接口一致
+            if (interfaceType != null && interfaceType.TypeArguments.Length > 0)
+            {
+                var genericArgName = classDeclaration.TypeParameterList.Parameters.FirstOrDefault()?.Identifier.Text;
+                var interfaceArgName = interfaceType.TypeArguments[0].Name;
+                return genericArgName != interfaceArgName;
+            }
+
+            return false;
         }
     }
 }
