891702e56355a04327a9c8bd72d7c24dab5efd1f
yueh0607 <imyueh@foxmail.com>
2023-08-28 09:36:21 +0800

    muti lan

---
+++ AirFramework.Unity/Assets/Framework/Runtime/UnityEnv/MutiLan/Components/MutiLanBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AirFramework
+{
+    public abstract class MutiLanBase :MonoBehaviour,ILanItem
+    {
+        [SerializeField]
+        protected string Key= string.Empty;
+
+
+        public abstract void Switch(string language);
+    }
+}
---
+++ AirFramework.Unity/Assets/Framework/Runtime/UnityEnv/MutiLan/Components/MutiLanImage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AirFramework
+{
+    public class MutiLanImage : MutiLanBase
+    {
+        [SerializeField] private Image target;
+        public override void Switch(string language)
+        {
+            
+        }
+    }
+}
